51739508a97bf53be7c4156b974e7faa2eb045af
Zak Grant <zak.d.grant@gmail.com>
2018-01-07 11:24:41 -0500

    handled auth

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
+
+//Passport config
+require('./config/passport')(passport);
+//Load Routes
+const auth = require('./routes/auth');
 
 const app = express();
 
@@ -8,6 +14,9 @@ const port = process.env.PORT || 5000;
 app.get('/',(req, res) => {
   res.send('Index');
 });
+
+//User Routes
+app.use('/auth', auth);
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:'mongodb://zak:123456@ds141657.mlab.com:41657/storybooks-dev-grant',
+  googleClientID:'1080651902413-918eq8n87ilqc1vbeofddgs3e4qo2n2a.apps.googleusercontent.com',
+  googleClientSecret: 'uIV0NGMfZicKjnrvIDYPxc4q'
+}
