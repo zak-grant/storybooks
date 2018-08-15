b407d8afcec89cca4fdecb68958300c9332030f8
Zak Grant <zak.d.grant@gmail.com>
2018-08-15 17:22:39 -0400

    initial commit

--- app.js
+++ app.js
@@ -1,22 +1,36 @@
 const express = require('express');
+const path = require('path');
 const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const passport = require('passport');
 
-//Load User Model
+//Load Models
 require('./models/User');
+require('./models/Story');
+
 //Passport config
 require('./config/passport')(passport);
 
 //Load Routes
 const index = require('./routes/index');
 const auth = require('./routes/auth');
+const stories = require('./routes/stories');
 
 //Load Keys File
 const keys = require('./config/keys');
 
+//Handlebars Helpers
+const {
+  truncate,
+  stripTags,
+  formatDate,
+  select,
+  editIcon
+} = require('./helpers/hbs');
 //Map Global Promises
 mongoose.Promise = global.Promise;
 
@@ -27,8 +41,22 @@ mongoose.connect(keys.mongoURI)
 
 const app = express();
 
+//Body Parser Middleware
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+//Method Override Middleware
+app.use(methodOverride('_method'));
+
 //Handlebars Middleware
 app.engine('handlebars', exphbs({
+  helpers:{
+    truncate: truncate,
+    stripTags: stripTags,
+    formatDate: formatDate,
+    select: select,
+    editIcon: editIcon
+  },
   defaultLayout:'main'
 }));
 app.set('view engine', 'handlebars');
@@ -50,9 +78,14 @@ app.use((req, res, next) => {
   next();
 });
 
+//Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
 //Use Routes
 app.use('/', index);
 app.use('/auth', auth);
+app.use('/stories', stories);
+
 
 const port = process.env.PORT || 5000;
 
